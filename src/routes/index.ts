53554e56aa221f156ed340c77f877e1367bd1138
Ernesto Sanchez <jose.e.molina2013@gmail.com>
2023-04-04 20:16:32 -0600

    login

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,13 @@
 import express from 'express';
 const router  = express.Router();
+import {validateKeyMiddleWare} from './middlewares/apikeyValidator';
+//import {validateJwtMiddleWare} from './middlewares/jwtValidator';
 
 router.get('/', (_req, res) => {
-  res.json({msg: 'Hello world'});
+  res.json({msg: 'Onlyfans ON the LINE'});
 });
+
+import securityRoutes from './security/security';
+router.use('/security', validateKeyMiddleWare, securityRoutes);
 
 export default router;
